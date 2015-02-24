7c6b44713a023b2edf93dbcb3d2fcc43a3034442
Marian Goia <mariangoia91@gmail.com>
2015-02-25 01:54:27 +0200

    Added user cell

---
+++ Interview/Views/Cells/IUserTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  IUserTableViewCell.h
+//  Interview
+//
+//  Created by Marian Goia on 25/02/15.
+//  Copyright (c) 2015 Marian Goia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IUserTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *statusIconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusMessageLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *userIconImageView;
+
+
+@end
---
+++ Interview/Views/Cells/IUserTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  IUserTableViewCell.m
+//  Interview
+//
+//  Created by Marian Goia on 25/02/15.
+//  Copyright (c) 2015 Marian Goia. All rights reserved.
+//
+
+#import "IUserTableViewCell.h"
+
+@implementation IUserTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
